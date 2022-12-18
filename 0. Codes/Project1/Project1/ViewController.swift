e31a6f9b6247f49fb3e81800f0e739c65e51df90
HoJunYang <didghwns0514@gmail.com>
2022-12-18 20:50:42 +0900

    doc(note.md):Update; 앱만들기 주요버튼, 화면소개2

---
+++ 0. Codes/Project1/Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by 양호준 on 2022/12/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
